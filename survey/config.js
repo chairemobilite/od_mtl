38daed055badbaa952756f2105b97eb848279342
Geneviève Bastien <gbastien@versatic.net>
2026-06-25 15:36:03 -0400

    Mise à jour d'Evolution
    fixes #167
    
    * Upgrade @playwright/test de 1.56.1 à 1.61.1
    * Ajout de l'option de configuration `accessCodeFormat` à la
    configuration, avec la valeur `0000-0000`.

--- survey/config.js
+++ survey/config.js
@@ -37,6 +37,7 @@ module.exports = {
     updateTransitRoutingIfCalculatedBefore: moment('2024-03-07').unix(), // timestamp, will recalculate transit trips if calculated before this date
     startButtonColor: 'turquoise', // styles for turquoise buttons are in the project's styles.scss file
     interviewableMinimumAge: 5,
+    accessCodeFormat: '0000-0000',
     selfResponseMinimumAge: 14,
     singlePersonInterview: false,
     allowChangeSectionWithoutValidation: true,
